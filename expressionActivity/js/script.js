6a9ffc05b90a362c7e7746d4dfc18e0243007216
jasonis <jislocum78@gmail.com>
2013-10-07 21:13:39 -0500

    modulo and order of operations

--- expressionActivity/js/script.js
+++ expressionActivity/js/script.js
@@ -16,3 +16,27 @@ var width = 4;
 var height = 5;
 var area = width * height/2;
 console.log(area);
+
+// Modulo Operator
+
+var remainder = 32 % 10;
+console.log(remainder);
+
+//  PEMDAS - the order of operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+// the average is the sum of the items dicided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
+
+
+

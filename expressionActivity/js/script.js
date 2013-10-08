2ca0ab90ea17fa67f6e454d3bf8f51f62add76eb
jasonis <jislocum78@gmail.com>
2013-10-07 20:45:12 -0500

    expression activity - arithmetic operators

---
+++ expressionActivity/js/script.js
@@ -0,0 +1,18 @@
+//  JavaScript
+
+var a = 2;
+b = a + 3;
+console.log(b);
+
+var yearBorn = 1978;
+var age = 2013 - yearBorn;
+console.log(age);
+
+// Arithmetic Operators
+
+// area of the triangle is half of the width times the height
+
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);

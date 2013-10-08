3780968d441d76e4279e2fa97ea0ad9461c1e9ca
jasonis <jislocum78@gmail.com>
2013-10-07 22:02:12 -0500

    problem 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,2 +1,12 @@
 // JavaScript
 // Jason Slocum 10-7-2013 Expressions Worksheet
+
+// Calculate the area of a rectangle is the width times the height
+
+var width = 7;  // the width of the rectangle
+var height = 5;  //  the height of the rectangle
+var result = width * height + " " + "sq ft";  //  the width of the rectangle times the height, in this case it should be 35sq ft
+console.log(result);  //  Print out the result of 7 * 5 = 35
+
+
+

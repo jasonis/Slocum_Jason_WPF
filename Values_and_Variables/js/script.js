ab78e30e3b578a7e4eca1a33e302a5382d680138
jasonis <jislocum78@gmail.com>
2013-10-07 19:42:11 -0500

    declaring, defining, string and boolean variables

--- Values_and_Variables/js/script.js
+++ Values_and_Variables/js/script.js
@@ -1,1 +1,16 @@
-/     JavaScript
+//     JavaScript
+
+var yearBorn = 1955;
+
+console.log(yearBorn)
+console.log("Hello World")
+
+alert("Hello World!")
+
+var yearOfBirth = 1978;
+
+console.log(yearOfBirth)
+
+var age = 35;
+var isStudent = false;
+var firstName = "Jason";

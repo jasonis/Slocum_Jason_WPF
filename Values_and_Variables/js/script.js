27d0ae3ca1970db5cd9fde5446e49dd6f2d36ceb
jasonis <jislocum78@gmail.com>
2013-10-07 20:01:31 -0500

    arrays

--- Values_and_Variables/js/script.js
+++ Values_and_Variables/js/script.js
@@ -14,3 +14,9 @@ console.log(yearOfBirth)
 var age = 35;
 var isStudent = false;
 var firstName = "Jason";
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
+console.log(muppetNames[0]);
+muppetNames[1] = "Fozzy";
+var num = 1;
+console.log(muppetNames[num]);

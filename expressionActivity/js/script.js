699b675bb1379c6bc27a9cda407e39e85180d0c1
jasonis <jislocum78@gmail.com>
2013-10-07 21:22:38 -0500

    expressions, arrays concatenating

--- expressionActivity/js/script.js
+++ expressionActivity/js/script.js
@@ -38,5 +38,27 @@ var width = 6;
 var perimeter = length * 2 + width * 2;
 console.log(perimeter);
 
+//  Expressions and Arrays
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+
+//  Concatenating Strings
+
+var firstName = "Jason";
+var lastName = "Slocum";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
+
+
+
 
 

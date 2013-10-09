ccac04f2696c1f956dffdf9a45550cd0edf53e16
jasonis <jislocum78@gmail.com>
2013-10-08 22:20:14 -0500

    comment

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -10,7 +10,7 @@ console.log("You ate " + slicesPerWeek + " slices of bread this week");  //this 
 var loavesPerWeek = slicesPerWeek / slicesPerLoaf;  //divide the slicesPerWeek by the slicesPerLoaf to find out how many loaves you ate in a week
 console.log("You ate " + loavesPerWeek + " loaves of bread this week");  //Printout for the total number of loaves you had in a week
 
-var loavesPerYear = loavesPerWeek * 52;
+var loavesPerYear = loavesPerWeek * 52;  //How many loaves you eat in a week times 52 weeks in a year
 console.log("You ate " + loavesPerYear + " loaves of bread this year");
 
 var daysPerLoaf = 365 / loavesPerYear;

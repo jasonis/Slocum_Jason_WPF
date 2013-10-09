b98be651d1ada20fda6d44478979a9bd4e3701b8
jasonis <jislocum78@gmail.com>
2013-10-08 22:40:38 -0500

    description and phone var

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,5 @@
 //  JavaScript
+//  What is the average monitor size in someone's home?
+//  This could be anything from smartphones and tablets to laptops, desktops and big screens
+
+var smartPhone = prompt("What is the screen size of your smart phone?");

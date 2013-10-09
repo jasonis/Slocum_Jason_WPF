8f0028699be2fde4f3b496ae7612de9bfd5ec6ad
jasonis <jislocum78@gmail.com>
2013-10-08 22:41:21 -0500

    tablet var

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,4 @@
 //  This could be anything from smartphones and tablets to laptops, desktops and big screens
 
 var smartPhone = prompt("What is the screen size of your smart phone?");
+var tablet + prompt("What is the screen size of your tablet?");

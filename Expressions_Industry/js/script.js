95dd3768a7b9c09ecba4772a26a7ba3aa9cecdeb
jasonis <jislocum78@gmail.com>
2013-10-08 22:42:23 -0500

    laptop prompt

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,4 +3,5 @@
 //  This could be anything from smartphones and tablets to laptops, desktops and big screens
 
 var smartPhone = prompt("What is the screen size of your smart phone?");
-var tablet + prompt("What is the screen size of your tablet?");
+var tablet = prompt("What is the screen size of your tablet?");
+var laptop = prompt("what is the screen size of your laptop?");

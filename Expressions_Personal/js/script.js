bc4ad7bc8c0a5e8be06e01fa248be575a2fa0c19
jasonis <jislocum78@gmail.com>
2013-10-08 21:42:54 -0500

    started writing vars

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,7 @@
 //  JavaScript
+
+//How many loaves of bread do I eat in a year?
+
+var slicesPerLoaf = prompt("How many slices are in your loaf of bread?");
+var slicesEatenDaily = prompt("How many slices do you eat in a day?");
+var slicesPerWeek = slicesEatenDaily * 7;

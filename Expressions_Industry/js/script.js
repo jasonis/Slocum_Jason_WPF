c0041309354464e03c0e8bcd84c7af517a7523e1
jasonis <jislocum78@gmail.com>
2013-10-08 22:53:37 -0500

    adding the array

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,3 +9,4 @@ var desktop = prompt("What is the screen size of your desktop?");
 var tv = prompt("What is the screen size of your smart TV?");
 var screenSizes = [smartPhone, tablet, laptop, desktop, tv];
 console.log(screenSizes);
+var totalSize = (screenSizes[0] + screenSizes[1] + screenSizes[2] + screenSizes[3] + screenSizes[4]);

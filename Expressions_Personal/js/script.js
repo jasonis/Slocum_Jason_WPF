f9d422efd0db0622608fb83ce5ddc97519af3a98
jasonis <jislocum78@gmail.com>
2013-10-08 21:53:41 -0500

    console printout

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,6 +5,6 @@
 var slicesPerLoaf = prompt("How many slices are in your loaf of bread?");
 var slicesEatenDaily = prompt("How many slices do you eat in a day?");
 var slicesPerWeek = slicesEatenDaily * 7;
-console.log("You ate " + slicesPerWeek + " of bread this week");
+console.log("You ate " + slicesPerWeek + " slices of bread this week");
 var loavesPerWeek = slicesPerWeek / slicesPerLoaf;
-console.log()
+console.log("You ate " + loavesPerWeek + " loaves of bread this week")

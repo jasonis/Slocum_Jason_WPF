5f970aab775a7ac8e74fd4ef3d8f3890fa5b6c32
jasonis <jislocum78@gmail.com>
2013-10-08 22:13:31 -0500

    started writing comments

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,13 +2,16 @@
 
 //How many loaves of bread do I eat in a year?
 
-var slicesPerLoaf = prompt("How many slices are in your loaf of bread?");
+var slicesPerLoaf = prompt("How many slices are in your loaf of bread?");  // How many slices are in your loaf of bread
 var slicesEatenDaily = prompt("How many slices do you eat in a day?");
 var slicesPerWeek = slicesEatenDaily * 7;
 console.log("You ate " + slicesPerWeek + " slices of bread this week");
+
 var loavesPerWeek = slicesPerWeek / slicesPerLoaf;
 console.log("You ate " + loavesPerWeek + " loaves of bread this week");
+
 var loavesPerYear = loavesPerWeek * 52;
 console.log("You ate " + loavesPerYear + " loaves of bread this year");
+
 var daysPerLoaf = 365 / loavesPerYear;
 console.log("You ate one loaf of bread every " + daysPerLoaf + " days");

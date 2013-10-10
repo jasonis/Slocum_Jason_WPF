3bbf4e8666e3bbee0ac13c7d8d13212429103b79
jasonis <jislocum78@gmail.com>
2013-10-09 19:57:42 -0500

    added duration var

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,3 +3,4 @@
 
 var currentLength = prompt("How long, in inches, is your current beard?");
 var growthRate = prompt("How long, in inches, does your beard grow in a week?");
+var duration = prompt("How many weeks will you be letting your beard grow?");

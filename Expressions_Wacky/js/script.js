c13c61c56cc54f441f345ae52ae34eed9861b6fc
jasonis <jislocum78@gmail.com>
2013-10-09 21:40:52 -0500

    added new constant

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,5 +7,6 @@ var duration = prompt("How many weeks will you be letting your beard grow?");// 
 var newGrowth = growthRate * duration;//  this will show how much new growth you have after your chosen length of time
 var result = Number(newGrowth) + Number(currentLength);// this will show what length your beard will be overall after your chosen amount of time new growth plus existing beard
 console.log("If I were to wait " + duration + " weeks, my beard would grow to a length of " + result + " inches.");// printout of how long your beard would be after the desired length of time
-var yearLength = growthRate * 52 + Number(currentLength);//  this is to figure out how long your beard would be if you left it alone for one year
+oneYear = 52;
+var yearLength = growthRate * oneYear + Number(currentLength);//  this is to figure out how long your beard would be if you left it alone for one year
 console.log("After one year your beard would be " + yearLength + " inches long");//  printout showing the total length that your beard would be after one year

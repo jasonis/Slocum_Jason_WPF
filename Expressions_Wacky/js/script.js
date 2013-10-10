92fe19734dbaee14c74f7054819e4d5b297a2212
jasonis <jislocum78@gmail.com>
2013-10-09 21:33:55 -0500

    added comment

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,7 +4,7 @@
 var currentLength = prompt("How long, in inches, is your current beard?");// How long is your beard currently if you have one already
 var growthRate = prompt("How long, in inches, does your beard grow in a week?");// How much would you say that your beard grows in a week?
 var duration = prompt("How many weeks will you be letting your beard grow?");// How long can you leave it alone before you cut it?
-var newGrowth = growthRate * duration;
+var newGrowth = growthRate * duration;//  this will show how much new growth you have after your chosen length of time
 var result = Number(newGrowth) + Number(currentLength);
 console.log("If I were to wait " + duration + " weeks, my beard would grow to a length of " + result + " inches.");
 var yearLength = growthRate * 52 + Number(currentLength);

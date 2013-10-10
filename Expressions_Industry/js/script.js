150c6d1d4b8a48aa8068f481177cf01b0cf9c9e0
jasonis <jislocum78@gmail.com>
2013-10-09 19:14:15 -0500

    added "in inches" to all of the text in the prompts

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,11 +4,11 @@
 //  We know how popular responsive design is and how it is great for viewing sites on many different size screens
 //  but what is the average size screen out of all of them we use all day?
 
-var smartPhone = prompt("What is the screen size of your smart phone?");
-var tablet = prompt("What is the screen size of your tablet?");
-var laptop = prompt("What is the screen size of your laptop?");
-var desktop = prompt("What is the screen size of your desktop?");
-var tv = prompt("What is the screen size of your smart TV?");
+var smartPhone = prompt("What is the screen size of your smart phone in inches?");
+var tablet = prompt("What is the screen size of your tablet in inches?");
+var laptop = prompt("What is the screen size of your laptop in inches?");
+var desktop = prompt("What is the screen size of your desktop in inches?");
+var tv = prompt("What is the screen size of your smart TV in inches?");
 var screenSizes = [smartPhone, tablet, laptop, desktop, tv];
 console.log(screenSizes);
 var totalSize = Number(smartPhone) + Number(tablet) + Number(laptop) + Number(desktop) + Number(tv);

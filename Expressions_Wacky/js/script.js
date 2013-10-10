f3f7682652c2be46d781833a2243456ce024f111
jasonis <jislocum78@gmail.com>
2013-10-09 19:56:10 -0500

    added first two vars

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,5 @@
 // JavaScript
+// The question at hand is "How long will my beard grow?"
+
+var currentLength = prompt("How long, in inches, is your current beard?");
+var growthRate = prompt("How long, in inches, does your beard grow in a week?");

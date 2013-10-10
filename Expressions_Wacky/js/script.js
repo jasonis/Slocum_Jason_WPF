49923f5ad376213e4ee6ce22b2f9d65cc4a80ab1
jasonis <jislocum78@gmail.com>
2013-10-09 21:14:14 -0500

    changed totalGrowth to newGrowth

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,6 +4,7 @@
 var currentLength = prompt("How long, in inches, is your current beard?");
 var growthRate = prompt("How long, in inches, does your beard grow in a week?");
 var duration = prompt("How many weeks will you be letting your beard grow?");
-var totalLength = growthRate * duration;
+var newGrowth = growthRate * duration;
 var result = Number(totalLength) + Number(currentLength);
 console.log("If I were to wait " + duration + " weeks, my beard would grow to a length of " + result + " inches.");
+ 

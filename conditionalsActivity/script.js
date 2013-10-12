c8ee5685c2104af6ee37cd1d666bb4191f5d48e1
jasonis <jislocum78@gmail.com>
2013-10-11 21:55:57 -0500

    following along with conditionals

---
+++ conditionalsActivity/script.js
@@ -0,0 +1,17 @@
+//JavaScript
+
+temp = 65;
+if (temp > 70){
+    goToTheBeach();
+}else{
+    goToTheMovies();
+}
+
+if(sunny){
+    goToTheBeach();
+    if(warmWater){
+        wearNewSuit();
+    }
+}else{
+    goToTheMovies();
+}

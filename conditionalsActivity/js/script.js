50e193de531d1bd439166d6697dcee25d58dc235
jasonis <jislocum78@gmail.com>
2013-10-12 17:35:27 -0500

    else if

--- conditionalsActivity/js/script.js
+++ conditionalsActivity/js/script.js
@@ -1,16 +1,20 @@
 //JavaScript
 
 
-var kidHeight = 47;
+var kidHeight = 44;
 var minHeight = 48;
+var wParentHeight = 45;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight> minHeight){
-    //code performed if condition is true
+if(kidHeight > minHeight){
+    //you can ride
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //cyou can ride with a parent
+    console.log("You can ride, but only with a parent");
 }else{
-    //code performed if condition is false
+    //sorry you have to grow
     console.log("Sorry kid, you've got some growing to do first!");
 }
 

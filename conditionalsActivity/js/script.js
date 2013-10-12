c59942eb919e53b03151f8151461749a1884bfc1
jasonis <jislocum78@gmail.com>
2013-10-12 15:37:41 -0500

    activity screencast

--- conditionalsActivity/js/script.js
+++ conditionalsActivity/js/script.js
@@ -1,10 +1,14 @@
 //JavaScript
 
 
-var oldEnough = true;
+var kedHeight = 50;
+var minHeight = 48;
 
 //if the child is old enough, print to the console "you can ride!"
-if(oldEnough){
+//if the kid is over 48 inches in height
+if(kedHeight > minHeight){
+    //code performed if condition is true
     console.log("You can ride the coaster!");
 }
+
 

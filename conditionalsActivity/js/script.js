03a732e9540142be2ae79135bf7b686ff12b6ffb
jasonis <jislocum78@gmail.com>
2013-10-12 18:17:45 -0500

    logical operators

--- conditionalsActivity/js/script.js
+++ conditionalsActivity/js/script.js
@@ -1,11 +1,15 @@
 //JavaScript
 
+
+
+
+
 var budget = 300;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the phone is less than our budget AND if our check is over 300
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery === true){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!!");

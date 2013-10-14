15d9e61a33063163c2147644a93229f0c97a2e90
jasonis <jislocum78@gmail.com>
2013-10-13 20:34:11 -0500

    users mpg var

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -8,4 +8,5 @@
 // Last Chance for Gas!
 
 var nextFillup = 200;// This is how many miles away the next gas station is
+var mpg = Number(prompt("How many miles per gallon does your car get?"));// Shows the users mpg
 

8596377809b934a342ed05979fd2c5a165b467cd
jasonis <jislocum78@gmail.com>
2013-10-13 20:35:30 -0500

    users current gas percentage

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -9,4 +9,4 @@
 
 var nextFillup = 200;// This is how many miles away the next gas station is
 var mpg = Number(prompt("How many miles per gallon does your car get?"));// Shows the users mpg
-
+var currentGas = Number(prompt("What percent of gas do you still have in your tank?"));//Shows the percentage of gas that is still in the users tank

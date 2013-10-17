19475a0718152f44173680955ef59eda7e4b1e6c
jasonis <jislocum78@gmail.com>
2013-10-16 19:43:03 -0500

    number of nights comment

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,7 +8,7 @@
 
 var roomCost = 100;//this is the given cost of a room for one night
 var occupants = Number(prompt("How many people will be staying in the room?"));//this prompts the user for the number of guests that will be using the room
-var nights = Number(prompt("How many nights will you be staying?"));
+var nights = Number(prompt("How many nights will you be staying?"));//this prompts the user to enter how many nights that they will be staying
 var earlyDiscount = prompt("Will you be booking your room more than two weeks in advance?");
 
 if(occupants == ""){

29e7407baf79ba36ba030f6bd7fc9ea47a403936
jasonis <jislocum78@gmail.com>
2013-10-16 21:41:48 -0500

    extrapages cost comment

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,7 +7,7 @@
 //Building a Website
 
 var standardSite = 200;//given cost for a standard site
-var extraPages = 50;
+var extraPages = 50;// this is how much extra it will cost if their site requires more than 5 pages
 var subTotal;
 var morePages = prompt("Will your site need more than 5 pages?");
 var total;

1669f351a70f0f0c78cce23b0d6ac537869f3385
jasonis <jislocum78@gmail.com>
2013-10-16 19:36:36 -0500

    validating number of guests prompt

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -10,3 +10,7 @@ var roomCost = 100;//this is the given cost of a room for one night
 var occupants = Number(prompt("How many people will be staying in the room?"));//this prompts the user for the number of guests that will be using the room
 var nights = Number(prompt("How many nights will you be staying?"));
 var earlyDiscount = prompt("Will you be booking your room more than two weeks in advance?");
+
+if(occupants == ""){
+    occupants = Number(prompt("How many people will be staying in the room?"));
+}

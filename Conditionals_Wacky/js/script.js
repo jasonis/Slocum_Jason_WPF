0f2724897c0a78ee0cac84411f85f56ed514d800
jasonis <jislocum78@gmail.com>
2013-10-16 20:20:25 -0500

    preferred odor prompt

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,3 +7,4 @@
 //What type of cheese should you try?
 
 var firmness = prompt("What is your preferred cheese texture?  soft, medium or hard");
+var odor = prompt("Do you prefer a mild cheese or a stinky cheese?");

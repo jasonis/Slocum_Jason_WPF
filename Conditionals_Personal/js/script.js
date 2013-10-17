920b8aa3a6d0ab3cdd7f05e1c85d548f636a3d30
jasonis <jislocum78@gmail.com>
2013-10-16 19:35:21 -0500

    number of nights prompt

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,3 +8,4 @@
 
 var roomCost = 100;//this is the given cost of a room for one night
 var occupants = Number(prompt("How many people will be staying in the room?"));//this prompts the user for the number of guests that will be using the room
+var nights = Number(prompt("How many nights will you be staying?"));

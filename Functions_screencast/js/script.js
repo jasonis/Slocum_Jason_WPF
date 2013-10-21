7e486595d29877e92623a0aaa9f9032b30de60e4
jasonis <jislocum78@gmail.com>
2013-10-20 22:26:16 -0500

    parameters and arguments

--- Functions_screencast/js/script.js
+++ Functions_screencast/js/script.js
@@ -1,2 +1,27 @@
 //          JavaScript          Functions Screencast
 
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+calcArea(30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
+    console.log(area);
+}
+
+function dogYears(){
+    var age = 4;
+    var dogYears = age * 7;
+    console.log("sparky is " + dogYears + " years old.");
+}
+dogYears();
+

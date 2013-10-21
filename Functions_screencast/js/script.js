c685e53ce5e17030097a2082c16b83913a71933f
jasonis <jislocum78@gmail.com>
2013-10-21 14:31:14 -0500

    following along with screencast

--- Functions_screencast/js/script.js
+++ Functions_screencast/js/script.js
@@ -4,24 +4,26 @@ function outptMsg(){
     console.log("Hello World");
 }
 
-function calcArea(){
+/*function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
     console.log(area);
-}
+}*/
 
-calcArea(30, 20);
+var total = calcArea(30, 20);
 
 function calcArea(w, h){
     var area = w * h;
-    console.log(area);
+    return area;
 }
+console.log(total);
 
-function dogYears(){
-    var age = 4;
+function dogYears(age){
     var dogYears = age * 7;
     console.log("sparky is " + dogYears + " years old.");
 }
-dogYears();
+dogYears(4);
+
+
 

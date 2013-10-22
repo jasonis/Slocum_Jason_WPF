6968cd4cde99656b0685289db264d5cc4b6657ad
jasonis <jislocum78@gmail.com>
2013-10-22 13:22:17 -0500

    created var for radius and return var for calcCirc function

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,3 +1,7 @@
 //     Jason Slocum          10.21.13          Functions Worksheet
 
 //  Calculate the circumference of a circle
+
+var radius = Number(prompt("Please enter the radius of the circle in inches"));
+
+var totalCirc = calcCirc(12);

fdb70632163d87636424d24c6d4a936cf741386f
jasonis <jislocum78@gmail.com>
2013-10-22 19:23:46 -0500

    created function comment

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -20,7 +20,7 @@ console.log("The circumference of the circle is " + totalCirc) + " inches";// th
 var victimLBS = Number(prompt("How many pounds does the victim weigh?"));// the number of pounds that the victim weighs
 var totalStings  = calcStings(victimLBS);// this is the result var that holds the argument for the function of calcStings
 
-function calcStings(victimLBS){
+function calcStings(victimLBS){// this declares the function calcStings and lists its parameter
     var stings = victimLBS * 8.666666667;
     return stings;
 }

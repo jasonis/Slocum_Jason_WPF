757b46117f67a2705c4483ff8819159614e15da0
jasonis <jislocum78@gmail.com>
2013-10-23 21:23:57 -0500

    var for the initial velocity

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,3 +5,4 @@
 //  Impulse Calculator.  The change in the momentum of an object caused over a very short period of time
 
 var m = Number(prompt("What is the mass of the object in question in kgs?"));
+var v = Number(prompt("What is the initial velocity of the object in m/s?"));

c476687918a73498cef65f7816b7ad935c6043de
jasonis <jislocum78@gmail.com>
2013-10-23 20:15:05 -0500

    height var comment

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,7 +6,7 @@
 
 var weightLoss;// variable for weightloss
 var weight = Number(prompt("What is your weight in pounds?"));// variable for how much the user weighs
-var height = Number(prompt("What is your height in inches?"));
+var height = Number(prompt("What is your height in inches?"));// variable for how tall the user is
 
 if(weight == ""){
     weight = Number(prompt("You have not entered your weight.  What is your weight in pounds?"));

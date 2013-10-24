58c7d28977438b45900f510adba51b7b40b8b148
jasonis <jislocum78@gmail.com>
2013-10-23 21:23:25 -0500

    var for mass

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,3 +3,5 @@
 //          JavaScript
 
 //  Impulse Calculator.  The change in the momentum of an object caused over a very short period of time
+
+var m = Number(prompt("What is the mass of the object in question in kgs?"));

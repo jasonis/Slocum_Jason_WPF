26c5a841dfefe2a52a456edb1bc2ef968d8fbdac
jasonis <jislocum78@gmail.com>
2013-10-23 20:42:18 -0500

    if fileSize ""

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,3 +6,7 @@
 
 var fileSize = Number(prompt("What is the size of the file you are downloading in megabytes?"));
 var speed = Number(prompt("What is the speed of your internet connection in megabits?"));
+
+if(fileSize == ""){
+    fileSize = Number(prompt("You seem to have left this field empty.  What is the size of the file you are downloading in megabytes?"));
+}

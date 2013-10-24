6bde1ae1b35b2a1a12579fcb198e2b239e105e21
jasonis <jislocum78@gmail.com>
2013-10-23 20:41:40 -0500

    speed var

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,4 @@
 //   Download time estimator
 
 var fileSize = Number(prompt("What is the size of the file you are downloading in megabytes?"));
+var speed = Number(prompt("What is the speed of your internet connection in megabits?"));
